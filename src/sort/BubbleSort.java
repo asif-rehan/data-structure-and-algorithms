f013dbb4d099c37e1a396a17f8a758c0368f4f2d
Asif Rehan <asif.rehan9@gmail.com>
2017-08-21 21:25:16 -0700

    feat: remove testing pieces from bubble sort class

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,40 +1,18 @@
-import java.util.Random;
+package sort;
 
 public class BubbleSort {
 	
-	
-	public static void main(String[] args){
-		/* randomly initiates an array of length below 10 and values between 1 to 9 
-		 * and sorts it from the smallest to largest
-		
-		pseudo code
+	public int[] Sort(int[] arr){
+		/*pseudo code
 		------------
 		for element from the left towards the right
 			for each element 
 			find the minimum out of everything to its right
 			swap the smallest at the current position
 		move to the next towards right
-		*/
-		
-		int[] arr = initializeRandomArray();
-		System.out.println("");
-		printElements(arr);
-		
-		sortArray(arr);
-		System.out.println("sorted");
-		printElements(arr);
-	}
-
-	private static int[] initializeRandomArray() {
-		Random rand = new Random();
-		Integer len = rand.nextInt(10);
-		int[] arr = rand.ints(len, 0, len).toArray();
-		System.out.print("len" + String.valueOf(len));
-		return arr;
-	}
-
-	private static void sortArray(int[] arr) {
-		//sort the array
+			 */
+		//sort the array using Bubble Sort algo
+	
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i+1; j < arr.length; j++) {
 				if (arr[i] > arr[j]){
@@ -44,12 +22,7 @@ public class BubbleSort {
 				}
 			}		
 		}
-	}
-
-	private static void printElements(int[] arr) {
-		for (int j = 0; j < arr.length; j++) {
-			System.out.println(arr[j]);
-		}
+	return arr;
 	}
 
 }
