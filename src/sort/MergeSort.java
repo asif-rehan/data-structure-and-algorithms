6510c85ff1f6663834217d13766afeb6518f9892
Asif Rehan <asif.rehan9@gmail.com>
2018-01-09 17:01:44 -0700

    work-in-progress: implementing merge sort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,5 +1,26 @@
 package sort;
 
+import java.util.ArrayList;
+
 public class MergeSort{
 	
+	public void sort(ArrayList<Integer> A){
+		sort(A, 0, A.size()-1); 
+	}
+
+	private void sort(ArrayList<Integer> a, int i, int j) {
+		if (i < j) {
+			int pivot_point = i + (j-1)/2;
+			sort(a, i, pivot_point);
+			sort(a, pivot_point+1, j);
+			merge(a, i, j, pivot_point);
+		}
+		
+		
+	}
+
+	private void merge(ArrayList<Integer> a, int i, int j, int pivot_point) {
+		
+		
+	}
 }
