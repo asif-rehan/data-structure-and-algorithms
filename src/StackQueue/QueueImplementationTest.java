5315aa4c873cfcdd7ba67a15b1f2377637696bba
Asif Rehan <asif.rehan9@gmail.com>
2020-10-20 22:11:28 -0400

    feat: add empty tests

---
+++ src/StackQueue/QueueImplementationTest.java
@@ -0,0 +1,36 @@
+package practiceJava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class QueueImplementationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		QueueImplementation<Integer> queue = new QueueImplementation<>();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	
+	}
+
+	@Test
+	void testAddToQueue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveFromFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPeek() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/StackQueue/StackImplementationTest.java
@@ -0,0 +1,39 @@
+package practiceJava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StackImplementationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPeek() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
